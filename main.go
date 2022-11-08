6d5c3024fa9eb6479aa024514a7333b79f3bb3c4
Akash JS <akashjs710@gmail.com>
2022-11-09 04:50:01 +0530

    template dockerized

--- main.go
+++ main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"fmt"
+	"html"
+	"html/template"
 	"log"
 	"net/http"
-
-	"github.com/jsakash/K8s-Sample/pkg/controllers"
 )
 
+// func init() {
+// 	gin.SetMode(gin.ReleaseMode)
+// }
+var tpl *template.Template
+
 func main() {
-	http.HandleFunc("/", controllers.Greet)
-	http.HandleFunc("/home", controllers.Home)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hi")
+	})
+	tpl, _ = tpl.ParseGlob("home.html")
+	http.HandleFunc("/home", home)
 	log.Println("Web server has started..")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "home.html", nil)
+
 }
